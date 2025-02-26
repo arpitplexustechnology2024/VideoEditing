a535b504575f6a57cfe4c3a84aaa1066be3ec021
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2025-02-26 09:39:17 +0530

    Initial Commit

---
+++ VideoEditing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoEditing
+//
+//  Created by Arpit iOS Dev. on 26/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VideoEditingTests/VideoEditingTests.swift
@@ -0,0 +1,17 @@
+//
+//  VideoEditingTests.swift
+//  VideoEditingTests
+//
+//  Created by Arpit iOS Dev. on 26/02/25.
+//
+
+import Testing
+@testable import VideoEditing
+
+struct VideoEditingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
